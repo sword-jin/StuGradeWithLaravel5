ac2f04e4f0fd9e2b5c4b710d688d91a19e45b83b
qq215672398 <215672398@qq.com>
2015-05-11 00:50:36 +0800

    PasswordController

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,39 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class PasswordController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller is responsible for handling password reset requests
+	| and uses a simple trait to include this behavior. You're free to
+	| explore this trait and override any methods you wish to tweak.
+	|
+	*/
+
+	use ResetsPasswords;
+
+	/**
+	 * Create a new password controller instance.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
+	 * @param  \Illuminate\Contracts\Auth\PasswordBroker  $passwords
+	 * @return void
+	 */
+	public function __construct(Guard $auth, PasswordBroker $passwords)
+	{
+		$this->auth = $auth;
+		$this->passwords = $passwords;
+		$this->subject = '学生成绩系统用户密码修改';
+		$this->redirectTo = '/';
+		$this->middleware('guest');
+	}
+
+}
